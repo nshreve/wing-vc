46b5163ff486a0dc0ef4c964dd8cb3d5996554c1
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-07-06 21:09:41 -0400

    Tabs component code

---
+++ tabs.js
@@ -0,0 +1,24 @@
+// Tabs Component
+{
+  const components = document.querySelectorAll("[data-tabs='component']");
+
+  components.forEach((c) => {
+    const tab = Array.from(c.querySelectorAll("[data-tabs='list'] > *"));
+    const visual = Array.from(c.querySelectorAll("[data-tabs='visual'] > *"));
+
+    const activate = (index) => {
+      const targetVisual = visual[index] ?? visual[0];
+      tab.forEach((el) => el.classList.remove("is-active"));
+      visual.forEach((el) => el.classList.remove("is-active"));
+      tab[index].classList.add("is-active");
+      targetVisual.classList.add("is-active");
+    };
+
+    tab.forEach((t, index) => {
+      t.addEventListener("click", () => activate(index));
+    });
+
+    // Init
+    activate(0);
+  });
+}
