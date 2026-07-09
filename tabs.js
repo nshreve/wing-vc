9da4f2d01fc29800a59412d0d34fd1062b59f1f1
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-07-09 09:25:54 -0400

    Update tabs visual code

--- tabs.js
+++ tabs.js
@@ -3,11 +3,13 @@
   const components = document.querySelectorAll("[data-tabs='component']");
 
   components.forEach((c) => {
-    const tab = Array.from(c.querySelectorAll("[data-tabs='list'] > *"));
+    const tab = Array.from(c.querySelectorAll("[data-tabs='item']"));
     const visual = Array.from(c.querySelectorAll("[data-tabs='visual'] > *"));
 
     const activate = (index) => {
-      const targetVisual = visual[index] ?? visual[0];
+      const targetVisual = visual[index]?.children.length
+        ? visual[index]
+        : visual[0];
       tab.forEach((el) => el.classList.remove("is-active"));
       visual.forEach((el) => el.classList.remove("is-active"));
       tab[index].classList.add("is-active");
