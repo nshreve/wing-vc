8c995d3d3eac836171370fa2298e97cdd8633551
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-07-13 15:42:36 -0400

    Switch tabs on hover

--- tabs.js
+++ tabs.js
@@ -6,9 +6,6 @@
     const tab = Array.from(c.querySelectorAll("[data-tabs='item']"));
     const visual = Array.from(c.querySelectorAll("[data-tabs='visual'] > *"));
 
-    let currentIndex = 0;
-    let autoplayTimer = null;
-
     const activate = (index) => {
       const targetVisual = visual[index]?.children.length
         ? visual[index]
@@ -17,31 +14,14 @@
       visual.forEach((el) => el.classList.remove("is-active"));
       tab[index].classList.add("is-active");
       targetVisual.classList.add("is-active");
-      currentIndex = index;
-    };
-
-    const stopAutoplay = () => {
-      if (autoplayTimer) {
-        clearInterval(autoplayTimer);
-        autoplayTimer = null;
-      }
-    };
-
-    const startAutoplay = () => {
-      autoplayTimer = setInterval(() => {
-        activate((currentIndex + 1) % tab.length);
-      }, 5000);
     };
 
     tab.forEach((t, index) => {
-      t.addEventListener("click", () => {
-        stopAutoplay();
-        activate(index);
-      });
+      t.addEventListener("mouseenter", () => activate(index));
+      t.addEventListener("click", () => activate(index));
     });
 
     // Init
     activate(0);
-    if (tab.length > 1) startAutoplay();
   });
 }
